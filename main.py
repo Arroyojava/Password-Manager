4ec2e58d365ecbe5c15b06183e888f6e5dbe8a21
Arroyojava <94626607+Arroyojava@users.noreply.github.com>
2023-08-17 19:06:40 -0400

    added and adjusted UI elements

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+FONT_NAME = 'Calibri'
+SIZE = 13
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -8,13 +10,39 @@ from tkinter import *
 # Window Config
 window = Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20)
+window.config(padx=25, pady=25)
 
 # Canvas / Background
-canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas = Canvas(width=325, height=200, highlightthickness=0)
 logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=0, row=0)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_label = Label(text='Website:', font=(FONT_NAME, SIZE))
+website_label.grid(column=0, row=1, sticky='E')
+
+email_username_label = Label(text='Email/Username:', font=(FONT_NAME, SIZE))
+email_username_label.grid(column=0, row=2, sticky='E')
+
+password_label = Label(text='Password:', font=(FONT_NAME, SIZE))
+password_label.grid(column=0, row=3, sticky='E')
+
+# Entry
+website_entry = Entry(width=35, font=(FONT_NAME, SIZE))
+website_entry.grid(column=1, row=1, sticky='w', padx=10, columnspan=2)
+
+email_username_entry = Entry(width=35, font=(FONT_NAME, SIZE))
+email_username_entry.grid(column=1, row=2, sticky='w', padx=10, columnspan=2)
+
+password_entry = Entry(width=19, font=(FONT_NAME, SIZE))
+password_entry.grid(column=1, row=3, sticky='w', padx=10, )
+# Buttons
+add_button = Button(text='Add', width=35, font=(FONT_NAME, SIZE))
+add_button.grid(column=1, row=4, sticky='w', padx=10, columnspan=2)
+
+gen_password_button = Button(text='Generate Password', font=(FONT_NAME, 12), width=16)
+gen_password_button.grid(column=1, row=3, sticky='E', columnspan=2, padx=10)
 
 window.eval('tk::PlaceWindow . Center')
 window.mainloop()
